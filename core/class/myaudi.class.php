6e34214b9314a12ee1ccf1c595d3783485568865
Mips2648 <mips2648@gmail.com>
2020-03-18 17:44:00 +0100

    add cron

--- core/class/myaudi.class.php
+++ core/class/myaudi.class.php
@@ -23,88 +23,88 @@ class myaudi extends eqLogic {
 
 	private static $PICTURES_DIR = __DIR__ . "/../pictures/";
 
-    private static function getCommandsConfig($file) {
-        $return = array();
-        $path = dirname(__FILE__) . "/../config/{$file}";
-        $content = file_get_contents($path);
-        if (is_json($content)) {
-            $return += json_decode($content, true);
-        } else {
-            log::add(__CLASS__, 'error', __('Fichier de configuration non trouvé:', __FILE__).$path);
-        }
+	private static function getCommandsConfig($file) {
+		$return = array();
+		$path = dirname(__FILE__) . "/../config/{$file}";
+		$content = file_get_contents($path);
+		if (is_json($content)) {
+			$return += json_decode($content, true);
+		} else {
+			log::add(__CLASS__, 'error', __('Fichier de configuration non trouvé:', __FILE__).$path);
+		}
 
-        return $return;
-    }
+		return $return;
+	}
 
-    public function createCmdFromDef($commandsDef) {
-        $link_cmds = array();
-        foreach ($commandsDef as $cmdDef){
-            $cmd = $this->getCmd(null, $cmdDef["logicalId"]);
-            if (!is_object($cmd)) {
-                log::add(__CLASS__, 'debug', 'create:'.$cmdDef["logicalId"].'/'.$cmdDef["name"]);
-                $cmd = new myaudiCmd();
-                $cmd->setLogicalId($cmdDef["logicalId"]);
-                $cmd->setEqLogic_id($this->getId());
-                $cmd->setName(__($cmdDef["name"], __FILE__));
-                if(isset($cmdDef["isHistorized"])) {
-                    $cmd->setIsHistorized($cmdDef["isHistorized"]);
-                }
-                if(isset($cmdDef["isVisible"])) {
-                    $cmd->setIsVisible($cmdDef["isVisible"]);
-                }
-                if (isset($cmdDef['template'])) {
-                    foreach ($cmdDef['template'] as $key => $value) {
-                        $cmd->setTemplate($key, $value);
-                    }
-                }
-            }
-            $cmd->setType($cmdDef["type"]);
-            $cmd->setSubType($cmdDef["subtype"]);
-            if(isset($cmdDef["generic_type"])) {
-                $cmd->setGeneric_type($cmdDef["generic_type"]);
-            }
-            if (isset($cmdDef['display'])) {
-                foreach ($cmdDef['display'] as $key => $value) {
-                    if ($key=='title_placeholder' || $key=='message_placeholder') {
-                        $value = __($value, __FILE__);
-                    }
-                    $cmd->setDisplay($key, $value);
-                }
-            }
-            if(isset($cmdDef["unite"])) {
-                $cmd->setUnite($cmdDef["unite"]);
-            }
+	public function createCmdFromDef($commandsDef) {
+		$link_cmds = array();
+		foreach ($commandsDef as $cmdDef){
+			$cmd = $this->getCmd(null, $cmdDef["logicalId"]);
+			if (!is_object($cmd)) {
+				log::add(__CLASS__, 'debug', 'create:'.$cmdDef["logicalId"].'/'.$cmdDef["name"]);
+				$cmd = new myaudiCmd();
+				$cmd->setLogicalId($cmdDef["logicalId"]);
+				$cmd->setEqLogic_id($this->getId());
+				$cmd->setName(__($cmdDef["name"], __FILE__));
+				if(isset($cmdDef["isHistorized"])) {
+					$cmd->setIsHistorized($cmdDef["isHistorized"]);
+				}
+				if(isset($cmdDef["isVisible"])) {
+					$cmd->setIsVisible($cmdDef["isVisible"]);
+				}
+				if (isset($cmdDef['template'])) {
+					foreach ($cmdDef['template'] as $key => $value) {
+						$cmd->setTemplate($key, $value);
+					}
+				}
+			}
+			$cmd->setType($cmdDef["type"]);
+			$cmd->setSubType($cmdDef["subtype"]);
+			if(isset($cmdDef["generic_type"])) {
+				$cmd->setGeneric_type($cmdDef["generic_type"]);
+			}
+			if (isset($cmdDef['display'])) {
+				foreach ($cmdDef['display'] as $key => $value) {
+					if ($key=='title_placeholder' || $key=='message_placeholder') {
+						$value = __($value, __FILE__);
+					}
+					$cmd->setDisplay($key, $value);
+				}
+			}
+			if(isset($cmdDef["unite"])) {
+				$cmd->setUnite($cmdDef["unite"]);
+			}
 
-            if (isset($cmdDef['configuration'])) {
-                foreach ($cmdDef['configuration'] as $key => $value) {
-                    $cmd->setConfiguration($key, $value);
-                }
-            }
+			if (isset($cmdDef['configuration'])) {
+				foreach ($cmdDef['configuration'] as $key => $value) {
+					$cmd->setConfiguration($key, $value);
+				}
+			}
 
-            if (isset($cmdDef['value'])) {
-                $link_cmds[$cmdDef["logicalId"]] = $cmdDef['value'];
-            }
+			if (isset($cmdDef['value'])) {
+				$link_cmds[$cmdDef["logicalId"]] = $cmdDef['value'];
+			}
 
-            $cmd->save();
+			$cmd->save();
 
-            if (isset($cmdDef['initialValue'])) {
-                $cmdValue = $cmd->execCmd();
-                if ($cmdValue=='') {
-                    $this->checkAndUpdateCmd($cmdDef["logicalId"], $cmdDef['initialValue']);
-                }
-            }
-        }
+			if (isset($cmdDef['initialValue'])) {
+				$cmdValue = $cmd->execCmd();
+				if ($cmdValue=='') {
+					$this->checkAndUpdateCmd($cmdDef["logicalId"], $cmdDef['initialValue']);
+				}
+			}
+		}
 
-        foreach ($link_cmds as $cmd_logicalId => $link_logicalId) {
-            $cmd = $this->getCmd(null, $cmd_logicalId);
-            $linkCmd = $this->getCmd(null, $link_logicalId);
+		foreach ($link_cmds as $cmd_logicalId => $link_logicalId) {
+			$cmd = $this->getCmd(null, $cmd_logicalId);
+			$linkCmd = $this->getCmd(null, $link_logicalId);
 
-            if (is_object($cmd) && is_object($linkCmd)) {
-                $cmd->setValue($linkCmd->getId());
-                $cmd->save();
-            }
-        }
-    }
+			if (is_object($cmd) && is_object($linkCmd)) {
+				$cmd->setValue($linkCmd->getId());
+				$cmd->save();
+			}
+		}
+	}
 
 	public static function deamon_info() {
 		$return = array();
@@ -167,6 +167,22 @@ class myaudi extends eqLogic {
 		system::kill('myaudid.py');
 		// system::fuserk(config::byKey('socketport', __CLASS__));
 		sleep(1);
+	}
+
+	public static function cron() {
+		foreach (eqLogic::byType(__CLASS__, true) as $eqLogic) {
+			if ($eqLogic->getIsEnable() != 1) continue;
+			$autorefresh = $eqLogic->getConfiguration('autorefresh', '');
+			if ($autorefresh == '')  continue;
+			try {
+				$cron = new Cron\CronExpression($autorefresh, new Cron\FieldFactory);
+				if ($cron->isDue()) {
+					$eqLogic->refresh();
+				}
+			} catch (Exception $e) {
+				log::add(__CLASS__, 'error', __('Expression cron non valide pour ', __FILE__) . $eqLogic->getHumanName() . ' : ' . $autorefresh);
+			}
+		}
 	}
 
 	public static function sendToDaemon($params) {
@@ -268,14 +284,17 @@ class myaudi extends eqLogic {
 		}
 		return '';
 	}
+
+	public function refresh() {
+		$params = array('method' => 'getVehicleData', 'vin' => $this->getLogicalId());
+		myaudi::sendToDaemon($params);
+	}
 }
 
 class myaudiCmd extends cmd {
 
 	public function execute($_options = array()) {
 		$eqlogic = $this->getEqLogic();
-
-		$params = array('method' => 'getVehicleData', 'vin' => $eqlogic->getLogicalId());
-		myaudi::sendToDaemon($params);
+		$eqlogic->refresh();
 	}
 }
