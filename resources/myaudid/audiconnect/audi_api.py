13c085897d839c319388bd16946a801e503cd57c
Milkshake <4212860+overmont@users.noreply.github.com>
2022-12-31 18:46:06 +0100

    Mise à jour des fichiers de la bibliothèque depuis arjenvrh

--- resources/myaudid/audiconnect/audi_api.py
+++ resources/myaudid/audiconnect/audi_api.py
@@ -19,8 +19,12 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class AudiAPI:
+    HDR_XAPP_VERSION = "4.13.0"
+    HDR_USER_AGENT = "myAudi-Android/4.13.0 (Build 800238275.2210271555) Android/11"
+
     def __init__(self, session, proxy=None):
         self.__token = None
+        self.__xclientid = None
         self._session = session
         if proxy is not None:
             self.__proxy = {"http": proxy, "https": proxy}
@@ -30,6 +34,9 @@ class AudiAPI:
     def use_token(self, token):
         self.__token = token
 
+    def set_xclient_id(self, xclientid):
+        self.__xclientid = xclientid
+
     async def request(
         self,
         method,
@@ -38,6 +45,7 @@ class AudiAPI:
         headers: Dict[str, str] = None,
         raw_reply: bool = False,
         raw_contents: bool = False,
+        rsp_wtxt: bool = False,
         **kwargs
     ):
         try:
@@ -47,6 +55,9 @@ class AudiAPI:
                 ) as response:
                     if raw_reply:
                         return response
+                    if rsp_wtxt:
+                        txt = await response.text()
+                        return response, txt
                     elif raw_contents:
                         return await response.read()
                     elif response.status == 200 or response.status == 202:
@@ -115,15 +126,16 @@ class AudiAPI:
 
     def __get_headers(self):
         data = {
-            "User-Agent": "okhttp/3.7.0",
-            "X-App-Version": "3.14.0",
+            "Accept": "application/json",
+            "Accept-Charset": "utf-8",
+            "X-App-Version": self.HDR_XAPP_VERSION,
             "X-App-Name": "myAudi",
-            "X-Market": "de_DE",
-            "Accept": "application/json"
-            # "Accept": "application/json, application/vnd.vwg.mbb.vehicleDataDetail_v2_1_0+xml, application/vnd.vwg.mbb.genericError_v1_0_2+xml",
+            "User-Agent": self.HDR_USER_AGENT,
         }
         if self.__token != None:
             data["Authorization"] = "Bearer " + self.__token.get("access_token")
+        if self.__xclientid != None:
+            data["X-Client-ID"] = self.__xclientid
 
         return data
 
