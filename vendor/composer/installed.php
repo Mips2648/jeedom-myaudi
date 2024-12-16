25e472c600225645e11581863aab0ebc1fdb7b5b
Mips2648 <mips2648@gmail.com>
2024-12-16 09:19:15 +0100

    restore vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mips/jeedom-myaudi',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'de91437a6b9dfcb7e4c5639d2a5ae0a1c718bc03',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mips/jeedom-myaudi' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'de91437a6b9dfcb7e4c5639d2a5ae0a1c718bc03',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mips/jeedom-tools' => array(
+            'pretty_version' => 'v1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => '5843ad156530cc36f40ff642baabd476219ddb6e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mips/jeedom-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
